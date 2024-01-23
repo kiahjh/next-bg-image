7588455f2c2b15d9d8a1f3c322aea857e3abb12f
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-01-23 16:59:24 -0500

    docs: switched to module dependency for next-bg-image instead of relative path

--- docs/app/demos/page.tsx
+++ docs/app/demos/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cx from 'classnames';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
-import NextBgImage, { bgColor } from '../../../next-bg-image/src';
+import NextBgImage, { bgColor } from 'next-bg-image';
 import { spaceGrotesk } from '@/lib/fonts';
 
 import FlowersImage from '@/public/flowers.jpg';
